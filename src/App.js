ed32111dede54ae8f02e7e9818616848e02b1477
VishakhaSNathan <vishakha.s@thoughtworks.com>
2024-05-01 15:45:22 +0530

    Vishakha | React spell checker

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SpellCheck from "./SpellCheckApp";
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <SpellCheck />
+    </div>
+  );
+}
+
+export default App;
